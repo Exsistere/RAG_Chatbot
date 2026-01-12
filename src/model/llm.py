4d98486193177feb0e791e7b5211964202ec3f8f
Drashya Sodha <drashya.sodha1@gmail.com>
2026-01-12 11:15:21 +0530

    stop tracking llm.py

--- src/model/llm.py
+++
@@ -1,6 +0,0 @@
-from google import genai
-from google.genai import types
-def ask_gemini(contents: str, system_prompt: str) -> str:
-    client = genai.Client(api_key="REMOVED")
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=contents, config= types.GenerateContentConfig(system_instruction=system_prompt))
-    return response
